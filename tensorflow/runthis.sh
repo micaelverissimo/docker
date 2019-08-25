2408b90b27f1a0f40d2ef7603d3119c3a7175abc
joaoVictorPinto <jodafons@cern.ch>
2019-08-25 18:53:57 +0200

    update

--- tensorflow/runthis.sh
+++ tensorflow/runthis.sh
@@ -1,1 +1,2 @@
-docker run --gpus all --network host -v jodafons:/volume -it jodafons/tensorflow /bin/bash
+#docker run --gpus all --network host -v jodafons:/volume -it jodafons/tensorflow /bin/bash
+docker run --network host -v jodafons:/volume -it jodafons/tensorflow /bin/bash
