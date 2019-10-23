e117bc8670b8e5d155de3255c0b1ceb179b2b505
joaoVictorPinto <jodafons@cern.ch>
2019-10-23 15:35:57 +0200

    update the gpu base image

--- gpu-base/setup_envs.sh
+++ gpu-base/setup_envs.sh
@@ -6,39 +6,33 @@ export LC_ALL=''
 export RCM_NO_COLOR=0
 export RCM_GRID_ENV=0
 export Athena_SETUP="off"
-
-
 # Set ROOT by hand
-export ROOTSYS="/opt/root_cern/build/"
+export ROOTSYS="/opt/root_cern/binary/"
 export PATH="$ROOTSYS/bin:$PATH"
 export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
-export PYTHONPATH="/opt/root_cern/build/lib:$PYTHONPATH"
+export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
+
 
 
 cd /code/saphyra/
-
+export SAPHYRA_PATH=`pwd`
 git pull
+mkdir .python_dir
+cd .python_dir
+mkdir python
+cd python
+ln -s ../../Gaugi/python Gaugi
+ln -s ../../saphyra/python saphyra
+ln -s ../../External/orchestra/python orchestra
+ln -s ../../External/ringerdb/python ringerdb
+ln -s ../../External/monet/python monet
+ln -s ../../External/pytex/python pytex
+ln -s ../../Tools/panda/python panda
+export PYTHONPATH=`pwd`:$PYTHONPATH
 
-# Set saphyra by hand
-cd build
-rm -rf lib
-mkdir lib
-for file in "`pwd`"/**/*.pcm
-do
-  echo "ln -sf $file lib"
-  ln -sf $file lib
-done 
 
-for file in "`pwd`"/**/*.so
-do
-  echo "ln -sf $file lib"
-  ln -sf $file lib
-done 
 
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=`pwd`:$LD_LIBRARY_PATH:$PYTHONPATH
-export PYTHONPATH=`pwd`/python:$PYTHONPATH
-export PATH=`pwd`/scripts:$PATH
+
 
 
 cd $CURRENT_DIR
